0e1f5734b451f7218c8784b62a453656376a3b70
Nithinmv <nithin8964@gmail.com>
2025-05-16 12:48:03 +0530

    shell script conditions loops example strted

--- 05.conditions.sh
+++ 05.conditions.sh
@@ -6,9 +6,9 @@ NUMBER=$1
 # -eq   --> equal
 # -ne   --> not equal
 
-if [ "$NUMBER" -lt 10 ]
+if [ $NUMBER -lt 10 ]
 then
-echo "Given $NUMBER is more then 10"
+echo "Given number $NUMBER is less then 10"
 else
-echo "Given $NUMBER is less then 10"
+echo "Given number $NUMBER is not less then 10"
 fi
