096e41a27a8ae035905f8bd09b3a27063dc73cb9
Nithinmv <nithin8964@gmail.com>
2025-05-16 11:21:36 +0530

    hello world started

---
+++ 04.modi_trump.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=modi
+PERSON2=trump
+echo "$PERSON1:: hey,trump,how u doing"
+echo "$PERSON2:: hi modi,fine,doing good"
+echo "$PERSON2:: what happend to pakisthan"
+echo "$PERSON1:: iam make sure that pak is not going to there in our global map"
