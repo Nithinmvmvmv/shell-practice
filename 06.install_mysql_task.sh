dfd08e708a157113c0a92ebaa5eccf33d74cb06b
Nithinmv <nithin8964@gmail.com>
2025-05-16 13:42:36 +0530

    mysql install task in progress

--- 06.install_mysql_task.sh
+++ 06.install_mysql_task.sh
@@ -5,20 +5,14 @@ if [ $USERID -nq 0 ]
 then
 echo "ERROR:: Please run this with root access"
 exit 1
-else
-echo "you running with root access"
-fi
-dnf list installed mysql
-if [ $? -ne 0]
-then 
-echo "mysql is not installed ---going to install now"
-dnf install mysql -y
-then 
-echo "Installing is success"
-else
-echo "Installing is failure"
-exit 1
-fi
-else
-echo "Mysql is alredy installed"
-fi
+
+     dnf list installed mysql
+     if [ $? -ne 0 ]
+     then 
+     echo "mysql is not installed ---going to install now"
+     dnf install mysql -y
+     fi
+     else
+     echo "Installing is failure"
+     exit 1
+     fi
