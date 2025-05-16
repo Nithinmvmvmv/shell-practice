181e14618b58c5deef8cf3625b71c79ff77ad767
Nithinmv <nithin8964@gmail.com>
2025-05-16 14:23:49 +0530

    using function and installing mysql python3 nginx

---
+++ 08-functions.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access"
+fi
+#using VALIDATE function to minimize the code
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "Installing $2 is ... SUCCESS"
+    else
+        echo "Installing $2 is ... FAILURE"
+        exit 1
+    fi
+}
+#giving installation command for mysql
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo "MySQL is already installed...Nothing to do"
+fi
+#giving installation command for python3
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is already installed...Nothing to do"
+fi
+##giving installation command for nginx
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed...Nothing to do"
+fi
