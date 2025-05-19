48236638e9238a5d9e44a39fb05b17b9353e0ac8
Nithinmv <nithin8964@gmail.com>
2025-05-19 14:05:25 +0530

    colours updated

--- 09-colours.sh
+++ 09-colours.sh
@@ -2,8 +2,13 @@
 
 #this line gives u colour and enables the colour,if u dont give end of colour then colour will apply for all lines
 echo -e "\e[31m hi,welcome"
-echo good morning
+echo "good morning"
+
+echo "-----------------------------------------"
 
 #to end the colour line we need to provide \e[0m
+#in this 1st line will be red and end ,
 echo -e "\e[31m hi,welcome \e[0m"
-echo good morning
+
+#this line prints normal white colour
+echo "Good morning"
