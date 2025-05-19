6db380df6673229e6be2637bb16936ee602770f5
Nithinmv <nithin8964@gmail.com>
2025-05-19 14:01:36 +0530

    colours for text started

--- 09-colours.sh
+++ 09-colours.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+#this line gives u colour and enables the colour,if u dont give end of colour then colour will apply for all lines
 echo -e "\e[31m hi,welcome"
+echo good morning
+
+#to end the colour line we need to provide \e[0m
+echo -e "\e[31m hi,welcome \e[0m"
 echo good morning
