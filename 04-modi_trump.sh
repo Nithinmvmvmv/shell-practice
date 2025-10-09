693565789c62987f49b41d7364c0c627fa60587b
Nithinmv <nithin8964@gmail.com>
2025-10-09 12:01:04 +0530

    git-dosa

--- 04-modi_trump.sh
+++ 04-modi_trump.sh
@@ -2,8 +2,10 @@
 
 PERSON1=modi
 PERSON2=trump
-echo "$PERSON1:: hey,trump,how u doing"
-echo "$PERSON2:: hi modi,fine,doing good"
+PERSON3=nithin
+echo "$PERSON1:: hey,$PERSON2,how u doing"
+echo "$PERSON2:: hi $PERSON1,fine,doing good"
 echo "$PERSON2:: what happend to pakisthan"
 echo "$PERSON1:: iam make sure that pak is not going to there in our global map"
 echo "$PERSON2:: pls give chance to talk with pak"
+echo "$PERSON3"::Hey welcome $PERSON1,$PERSON2
