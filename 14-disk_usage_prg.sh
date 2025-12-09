f63e9101d5a0939667506c8fc4a680672f575b5a
Nithinmv <nithin8964@gmail.com>
2025-12-09 12:42:33 +0530

    disk usage

--- 14-disk_usage_prg.sh
+++ 14-disk_usage_prg.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Printing the disk usage or less then 80% disk used files"
-df -h | awk 'NR>1 && int($5) < 80 { print $6 "is" $5 "not full"}'
+df -h | awk 'NR>1 && int($5) < 80 { print $6  "  is"  $5  "  not full"}'
 
 
 echo "disk usage"
 
 echo "Printing file systems where disk usage is more than 80%"
 
-df -h | awk 'NR>1 && int($5) > 80 { print $6 " is " $5 " full" }'
+df -h | awk 'NR>1 && int($5) > 80 { print $6  "  is "  $5 "  full" }'
